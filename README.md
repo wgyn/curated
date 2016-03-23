5f0a0b16ba4788486a23423030bbb98baec5a18a
Ryan Wang <wgyn888@gmail.com>
2016-03-22 23:37:09 -0500

    Use webpack for bundling

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ To setup:
 To run locally:
 - Start a local mongod: `sudo mongod`
 - Start the backend: `bundle exec ruby app.rb`
+- Build the frontend: `webpack`
--- public/scripts/views.jsx
+++ public/scripts/views.jsx
@@ -1,3 +1,6 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
 var ReadingListContainer = React.createClass({
   loadData: function() {
     $.ajax({
