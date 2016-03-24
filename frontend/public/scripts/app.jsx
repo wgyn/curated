d650b7d0f6c7dcfbd06757fe1220bac8cdbc844d
Ryan Wang <wgyn888@gmail.com>
2016-03-23 23:11:54 -0500

    Use browserHistory instead of hashHistory

--- frontend/public/scripts/app.jsx
+++ frontend/public/scripts/app.jsx
@@ -1,13 +1,12 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
-var {Route, Router, hashHistory} = require('react-router')
+var {Route, Router, browserHistory} = require('react-router')
 var ReadingListContainer = require('./views.jsx')
 
 var routes = (
-  <Router history={hashHistory}>
-    <Route path="/" component={ReadingListContainer}>
-      <Route path="/lists/:id" component={ReadingListContainer} />
-    </Route>
+  <Router history={browserHistory}>
+    <Route path="/" component={ReadingListContainer} />
+    <Route path="/lists/:id" component={ReadingListContainer} />
   </Router>
 );
 
